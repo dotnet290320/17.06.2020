c43105a9aa0eacd5edfc6e1ee8ae76b52a814b62
dotnet290320 <62850561+dotnet290320@users.noreply.github.com>
2020-06-17 19:57:38 +0300

    Add files via upload

---
+++ class_func/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _170620a
+{
+    class Circle
+    {
+        public double _radius;
+
+        public override string ToString()
+        {
+            return $"Circle [radius]:{_radius}";
+        }
+    }
+}
