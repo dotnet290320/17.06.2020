548c821f99db4de8f632db391a83397690a1747f
dotnet290320 <62850561+dotnet290320@users.noreply.github.com>
2020-06-17 19:24:35 +0300

    Add files via upload

---
+++ hw_solution/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _170620
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer myMac = new Computer()
+            {
+                model = "IOS 10",
+                numberOfProcessors = 8,
+                price = 10000,
+                isTurnOn = true
+            };
+            myMac.screenSize = 24;
+
+            Console.WriteLine(myMac);
+
+            Computer MyHP = new Computer()
+            {
+                model = "HP 8000",
+                numberOfProcessors = 16,
+                price = 7000,
+                screenSize = 28
+            };
+
+            Console.WriteLine(MyHP);
+
+            MyHP.TellMeThePrice(); // void
+
+            MyHP.TellMeTheScreenSize(); // this is bad ... nothing will happen
+
+            // 1. good - create variable to store the screen size , and print it
+            float sizeOfMyHP = MyHP.TellMeTheScreenSize();
+            Console.WriteLine(sizeOfMyHP);
+
+            // 2. good - print the screen size return value
+            Console.WriteLine(MyHP.TellMeTheScreenSize());
+
+            // 3.
+            Console.WriteLine(MyHP.screenSize);
+
+            MyHP.AddProcessor(8);
+
+            MyHP.TurnOn();
+            MyHP.TurnOn();
+
+            Console.WriteLine(MyHP);
+
+        }
+    }
+}
